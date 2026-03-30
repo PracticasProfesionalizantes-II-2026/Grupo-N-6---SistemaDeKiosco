b8a8ee6b32070d43661cf033b8a1eb2eabc9eeeb
AgustinCravero <agucrave13@gmail.com>
2026-03-30 20:42:09 -0300

    Actualizacion

---
+++ Clases-KioPlus/Models/Caja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_KioPlus.Models
+{
+    public class Caja
+    {
+        public int Id { get; set; }
+
+        // Monto total actual de dinero en la caja del sistema
+        public double Monto { get; set; }
+
+    }
+}
---
+++ Clases-KioPlus/Models/Venta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_KioPlus.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        [Required] public DateTime FechaHora { get; set; }
+
+        // Relación con el usuario que realizó la venta
+        [Required] public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        [Required] public double MontoTotal { get; set; }
+
+        // Relación con la cuenta corriente del cliente (si aplica)
+        [Required] public int CuentaCorrienteClienteId { get; set; }
+        public CuentaCorrienteCliente CuentaCorrienteCliente { get; set; }
+
+        // Relación con la forma de pago utilizada en la venta
+        [Required] public int FormaPagoId { get; set; }
+        public FormaPago FormaPago { get; set; }
+        public DateTime FechaPago { get; set; }
+
+        // Estado actual de la venta (Pagado / NoPagado)
+        [Required] public EstadoVenta Estado { get; set; }
+        public enum EstadoVenta
+        {
+            Pagado,  // venta pagada
+            NoPagado // venta no pagada
+        }
+
+        // Relación uno a muchos con DetalleVenta
+        public List<DetalleVenta> DetallesVenta { get; set; }
+    }
+}
